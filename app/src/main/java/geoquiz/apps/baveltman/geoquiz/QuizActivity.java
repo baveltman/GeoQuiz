92c13e57abd586bacfade7c36a2030fe66a5f25b
baveltman <baveltman@gmail.com>
2015-02-27 13:03:17 -0600

    Dynamic geoquiz with question bank

--- app/src/main/java/geoquiz/apps/baveltman/geoquiz/QuizActivity.java
+++ app/src/main/java/geoquiz/apps/baveltman/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 
@@ -13,34 +14,107 @@ public class QuizActivity extends ActionBarActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        //get references to the inflated view objects for the buttons
-        mTrueButton = (Button)findViewById(R.id.true_button);
-        mFalseButton = (Button)findViewById(R.id.false_button);
+        BindButtons();
+        BindFirstQuestion();
+        BindButtonListeners();
 
+    }
+
+    /*
+    Sets up listeners for true, false, and next buttons
+     */
+    private void BindButtonListeners() {
         //set onclick listeners for buttons
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                ++mCurrentIndex;
+                if (mCurrentIndex >= mQuestionBank.length){
+                    mCurrentIndex = 0;
+                }
+                updateQuestion();
+            }
+
+        });
+    }
+
+    /**
+     * evaluates whether the user has selected the correct answer and makes a toast accordingly
+     * @param answer is the answer, true or false, selected by the user
+     */
+    private void checkAnswer(boolean answer){
+
+        int messageId = 0;
+
+        if (mQuestionBank[mCurrentIndex].isTrueQuestion() == answer){
+            messageId = R.string.correct_toast;
+        } else {
+            messageId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(QuizActivity.this,
+                messageId,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+    updates to the next question in the question bank
+     */
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    /*
+    Creates and binds the first question for the geoquiz game
+     */
+    private void BindFirstQuestion() {
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        updateQuestion();
+    }
+
+
+    /*
+    binds references to the true, false, and next buttons
+     */
+    private void BindButtons(){
+        //get references to the inflated view objects for the buttons
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mNextButton = (Button)findViewById(R.id.next_button);
     }
 
 
